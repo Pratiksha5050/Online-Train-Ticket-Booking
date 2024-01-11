5acea78cb58e59111691eeb81697cd41501a70f5
Pratiksha Kalokhe <71497965+Pratiksha5050@users.noreply.github.com>
2024-01-11 12:19:36 +0530

    Delete TrainBooking directory

--- TrainBooking/src/main/java/com/example/TrainBooking/repository/TrainBookingRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.TrainBooking.repository;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-
-import com.example.TrainBooking.entity.TrainBooking;
-import java.util.*;
-public interface TrainBookingRepo extends MongoRepository<TrainBooking, String>{
-	
-}
