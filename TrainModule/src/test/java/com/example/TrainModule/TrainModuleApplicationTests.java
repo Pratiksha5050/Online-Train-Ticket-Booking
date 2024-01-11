d724f4c217d764b91a65befdced186804fa51bad
Pratiksha Kalokhe <71497965+Pratiksha5050@users.noreply.github.com>
2024-01-11 09:56:17 +0530

    Add files via upload

---
+++ TrainModule/src/test/java/com/example/TrainModule/TrainModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TrainModule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
