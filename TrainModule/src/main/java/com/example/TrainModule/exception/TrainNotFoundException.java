62cdf87ad82598544b31f32dc2196e6b55fc06b1
Pratiksha Kalokhe <71497965+Pratiksha5050@users.noreply.github.com>
2024-01-15 15:03:52 +0530

    Delete TrainModule directory

--- TrainModule/src/main/java/com/example/TrainModule/exception/TrainNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.TrainModule.exception;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(org.springframework.http.HttpStatus.NOT_FOUND)
-public class TrainNotFoundException extends RuntimeException {
-    public TrainNotFoundException(String trainNo) {
-        super("Train not found with id: " + trainNo);
- }
-}
